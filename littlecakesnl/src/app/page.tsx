c94c2656124a4f044674cd0d0b2107b3f4935d99
Usmansiddiqui <usman-ack@hotmail.com>
2025-01-18 21:22:52 +0100

    Update image for mobile screen size

--- littlecakesnl/src/app/page.tsx
+++ littlecakesnl/src/app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div className="flex min-h-screen">
-      {/* Left Side: 60% width with light pink background */}
-      <div className="w-3/5 bg-pink-100 flex items-center justify-center p-10">
+    <div className="flex flex-wrap min-h-screen">
+      {/* Left Side: 60% width on large screens, full width on smaller screens */}
+      <div className="w-full lg:w-3/5 bg-pink-100 flex items-center justify-center p-10">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-gray-900 mb-6">LittleCakesNL</h1>
           <p className="text-xl text-pink-700 mb-1 font-extrabold">Delicious custom cakes in The Netherlands</p>
@@ -15,8 +15,8 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Right Side: 40% width with the image */}
-      <div className="w-2/5">
+      {/* Right Side: 40% width on large screens, hidden on smaller screens */}
+      <div className="hidden lg:block lg:w-2/5">
         <img
           src="/cakesrc.png"
           alt="Cake"
